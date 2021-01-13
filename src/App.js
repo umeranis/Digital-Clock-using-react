25769119956bc3191a05be63fdd93142df21ffc2
Umer Anis <umeranis24@gmail.com>
2021-01-13 21:32:41 +0500

    Digital Clock

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+
+
+const App = () => {
+
+  let time = new Date().toLocaleTimeString();
+  const [ctime, setctime] = useState(time);
+
+  const UpdateTime = () => {
+    time = new Date().toLocaleTimeString();
+    setctime(time);
+  };
+  setInterval(UpdateTime,1000)
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="why">
+    <h1 className="head">Time</h1>
+    <h2 className="time"> {ctime} </h2>
     </div>
   );
 }
